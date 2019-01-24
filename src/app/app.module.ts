dd8f1143f605597940fe0a59de9c85536ae426fb
Riyan Nur Rizal <riyannurrizal@gmail.com>
2019-01-24 15:29:37 +0700

    penambahan Validasi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +7,8 @@ import { BukuComponent } from './buku/buku.component';
 import { BukuAddComponent } from './buku/buku-add/buku-add.component';
 import { BukuListComponent } from './buku/buku-list/buku-list.component';
 import { HoverHighlightDirective } from './hover-highlight.directive';
+import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
@@ -14,12 +16,15 @@ import { HoverHighlightDirective } from './hover-highlight.directive';
     BukuComponent,
     BukuAddComponent,
     BukuListComponent,
-    HoverHighlightDirective
+    HoverHighlightDirective,
+    ContactComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/buku/buku-add/buku-add.component.ts
+++ src/app/buku/buku-add/buku-add.component.ts
@@ -1,6 +1,7 @@
-import {Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { Buku } from '../buku.model';
 import { BukusService } from '../bukus.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-buku-add',
@@ -9,6 +10,8 @@ import { BukusService } from '../bukus.service';
   providers: [BukusService]
 })
 export class BukuAddComponent implements OnInit {
+  bukuForm: FormGroup;
+
 
   @ViewChild('inputQty') inputQty: ElementRef;
 
@@ -20,20 +23,42 @@ export class BukuAddComponent implements OnInit {
   constructor(private bukuServis: BukusService) { }
 
   ngOnInit() {
+    this.bukuForm = new FormGroup({
+      isbn: new FormControl(null, [Validators.required, this.cekIsEmpty]),
+      nama: new FormControl(null, [Validators.required, this.cekIsEmpty]),
+      qty: new FormControl(null, [Validators.required, this.cekIsEmpty])
+    });
   }
 
-  tambahBuku(inputEmail: HTMLInputElement){
+  tambahBuku(inputEmail: HTMLInputElement) {
     this.bukuAdded.emit(this.bukuServis.convertBuku(this.inputInfo));
     this.inputInfo = new Buku();
-  }
-/** 
-  tambahBuku(inputEmail: HTMLInputElement) {
-    console.log(this.inputInfo);
-    console.log(inputEmail.value);
-    console.log(this.inputContact.nativeElement.value);
 
-    this.bukuAdded.emit(this.inputInfo);
-    this.inputInfo = new Buku();
   }
-*/
+
+  kirim() {
+    const bukuUsModel = new Buku();
+    bukuUsModel.isbn = this.bukuForm.get('isbn').value;
+    bukuUsModel.nama = this.bukuForm.get('nama').value;
+    bukuUsModel.qty = this.bukuForm.get('qty').value;
+    console.log(bukuUsModel);
+  }
+
+  cekIsEmpty(control: FormControl): { [s: string]: boolean } {
+    if (control.value && control.value.trim().length === 0) {
+      return { 'blank': true };
+    }
+    return null;
+  }
+
+  /** 
+    tambahBuku(inputEmail: HTMLInputElement) {
+      console.log(this.inputInfo);
+      console.log(inputEmail.value);
+      console.log(this.inputContact.nativeElement.value);
+  
+      this.bukuAdded.emit(this.inputInfo);
+      this.inputInfo = new Buku();
+    }
+  */
 }
