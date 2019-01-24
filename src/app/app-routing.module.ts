573e523fd48e358eb454f8cf52c02f839ad3d54d
Riyan Nur Rizal <riyannurrizal@gmail.com>
2019-01-24 15:59:31 +0700

    penambahan links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BukuComponent } from './buku/buku.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'buku', component: BukuComponent },
+  { path: 'contact', component: ContactComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
